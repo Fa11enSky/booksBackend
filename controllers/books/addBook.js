7f85eedde74e940c86911bf356eba3d5c92ef9ba
Fa11enSky <iwanpomirsky@gmail.com>
2024-08-30 01:06:57 +0300

    added the ability to add a book

--- controllers/books/addBook.js
+++ controllers/books/addBook.js
@@ -1,4 +1,11 @@
+const addBookSrv = require("../../DBServices/addBookSrv")
+const { errorsCreator } = require("../../helpers")
+
 const addBook = async(req,res) => {
-  
+    const result = await addBookSrv(req.body)
+    if (!result) {
+        throw errorsCreator(500)
+    }
+    res.status(201).json(result)
 }
 module.exports = addBook
--- routes/books.js
+++ routes/books.js
@@ -1,12 +1,13 @@
 const express = require('express')
-const { getAllBooks, deleteBook, updateBook } = require('../controllers')
+const { getAllBooks, deleteBook, updateBook, addBook } = require('../controllers')
 const validateBody = require('../helpers/validateBody')
-const { updateBookSchema } = require('../schemas/bookSchema')
+const { updateBookSchema, createBookSchema } = require('../schemas/bookSchema')
 
 const booksRouter = express.Router()
 
 
 booksRouter.get('/', getAllBooks)
 booksRouter.delete('/:isbn', deleteBook)
-booksRouter.put('/:isbn',validateBody(updateBookSchema),updateBook)
+booksRouter.put('/:isbn', validateBody(updateBookSchema), updateBook)
+booksRouter.post('/',validateBody(createBookSchema),addBook)
 module.exports = booksRouter
