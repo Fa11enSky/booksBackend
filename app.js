d536d68c0e86352525a57376797b51d356e784de
Fa11enSky <iwanpomirsky@gmail.com>
2024-08-30 00:00:22 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors');
+const booksRouter = require('./routes/books');
+
+const app = express()
+
+app.use(cors());
+app.use(express.json())
+
+app.use('/books',booksRouter)
+app.use((_, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+app.listen(3000)
